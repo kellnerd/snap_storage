1a2565113841a925b7a40bcc06b2f083a77c6b39
David Kellner <52860029+kellnerd@users.noreply.github.com>
2023-06-25 14:58:12 +0200

    doc: Annotate important snapshot functions and types

--- src/snapshot.ts
+++ src/snapshot.ts
@@ -2,18 +2,23 @@ import { assert, crypto, dirname, joinPath, toHashString } from "./deps.ts";
 
 export type Content = Uint8Array | ReadableStream<Uint8Array>;
 
+/** Policy which is used to filter snapshots. */
 export type Policy = {
   /** Maximum age of the snapshot in seconds. */
   maxAge?: number;
 };
 
+/** Metadata of a snapshot for an URI. */
 export type Snapshot = {
   /** Creation date and time in seconds since the UNIX epoch. */
   timestamp: number;
+  /** Hash of the content. Used to determine the path. */
   contentHash: string;
+  /** Path to the content. */
   path: string;
 };
 
+/** Validates whether the given snapshot follows the given policy. */
 export function followsPolicy(snap: Snapshot, policy: Policy) {
   if (policy.maxAge) {
     return now() < snap.timestamp + policy.maxAge;
@@ -21,6 +26,7 @@ export function followsPolicy(snap: Snapshot, policy: Policy) {
   return true;
 }
 
+/** Calculates a hash over the given content. */
 export async function hash(content: Content): Promise<string> {
   const contentHash = await crypto.subtle.digest("SHA-256", content);
   return toHashString(contentHash);
@@ -28,6 +34,7 @@ export async function hash(content: Content): Promise<string> {
 
 export const hashLength = 64;
 
+/** Determines the path to a snapshot based on the hash of its content. */
 export function snapPath(basePath: string, contentHash: string): string {
   assert(contentHash.length === hashLength);
   const dirNameLength = 2;
@@ -40,6 +47,7 @@ export function snapPath(basePath: string, contentHash: string): string {
   );
 }
 
+/** Writes a new snapshot with the given content and returns its metadata. */
 export async function writeSnap(
   basePath: string,
   content: Content | string,
