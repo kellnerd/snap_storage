45bc7d9ca88e900cb09bab5f5203d452b174173a
David Kellner <52860029+kellnerd@users.noreply.github.com>
2024-10-21 21:28:01 +0200

    chore(deps): Update https://deno.land/std to 0.224
    Last version which is available on deno.land before the move to jsr.io

--- src/deps.ts
+++ src/deps.ts
@@ -1,13 +1,13 @@
 // standard library
-export { delay } from "https://deno.land/std@0.206.0/async/delay.ts";
-export { crypto } from "https://deno.land/std@0.206.0/crypto/mod.ts";
-export { encodeHex } from "https://deno.land/std@0.206.0/encoding/hex.ts";
+export { delay } from "https://deno.land/std@0.224.0/async/delay.ts";
+export { crypto } from "https://deno.land/std@0.224.0/crypto/mod.ts";
+export { encodeHex } from "https://deno.land/std@0.224.0/encoding/hex.ts";
 export {
   dirname,
   join as joinPath,
   resolve,
   toFileUrl,
-} from "https://deno.land/std@0.206.0/path/mod.ts";
+} from "https://deno.land/std@0.224.0/path/mod.ts";
 
 // third-party
 export {
@@ -18,11 +18,11 @@ export {
 } from "https://deno.land/x/sqlite@v3.9.1/mod.ts";
 
 // testing
-export { assert } from "https://deno.land/std@0.206.0/assert/assert.ts";
-export { assertEquals } from "https://deno.land/std@0.206.0/assert/assert_equals.ts";
+export { assert } from "https://deno.land/std@0.224.0/assert/assert.ts";
+export { assertEquals } from "https://deno.land/std@0.224.0/assert/assert_equals.ts";
 export {
   afterAll,
   beforeAll,
   describe,
   it,
-} from "https://deno.land/std@0.206.0/testing/bdd.ts";
+} from "https://deno.land/std@0.224.0/testing/bdd.ts";
